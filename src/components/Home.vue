12b049866ced46ffd69ba196bf0cca724ed58d22
v_yazzzhang <v_yazzzhang@tencent.com>
2020-07-02 20:51:43 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>头部</h1>
+        <span>{{msg}}</span>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            msg: 'hello insdzfddex.vue'
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
 
